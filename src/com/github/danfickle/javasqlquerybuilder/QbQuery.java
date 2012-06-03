c6213872435602ffebcfb95181b4ba51e17d7af2
Dan Fickle <newtab@gmail.com>
2012-06-03 20:36:21 +1000

    Added QbQuery interface, the parent interface for select, insert, update and delete queries.

---
+++ src/com/github/danfickle/javasqlquerybuilder/QbQuery.java
@@ -0,0 +1,25 @@
+package com.github.danfickle.javasqlquerybuilder;
+
+/**
+ * Parent interface for a select, update, insert or delete query.
+ * QbQuery child interfaces should be created using a DB
+ * specific Factory instance.
+ * @author DanFickle
+ */
+public interface QbQuery
+{
+	/**
+	 * Gets the created query string that needs to be passed to 
+	 * Connection.prepareStatement(String).
+	 * @return The query string.
+	 */
+	public String getQueryString();
+	
+	/**
+	 * Gets the index of a placeholder.
+	 * @param placeholderName - The name of the placeholder value.
+	 * @return An integer that can be passed to
+	 * PreparedStatement::set* functions.
+	 */
+	public int getPlaceholderIndex(String placeholderName);
+}
