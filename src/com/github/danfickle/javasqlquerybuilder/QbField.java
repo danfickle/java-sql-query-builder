59b77cb6dba88d9b79815328f10253032b44aabe
Dan Fickle <newtab@gmail.com>
2012-06-03 19:48:51 +1000

    Added QbField interface - deliberately an empty interface.

---
+++ src/com/github/danfickle/javasqlquerybuilder/QbField.java
@@ -0,0 +1,11 @@
+package com.github.danfickle.javasqlquerybuilder;
+
+/**
+ * A QbField can be used in select and where clauses and represents
+ * a database field or an aggregate function on a database field.
+ * Use a DB specific Factory to create QbField objects.  
+ * @author DanFickle
+ */
+public interface QbField
+{
+}
