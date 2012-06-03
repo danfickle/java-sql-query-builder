1231ecb5e2f7c2b773d006ea588dd49fa0a138dc
Dan Fickle <newtab@gmail.com>
2012-06-03 22:19:37 +1000

    Added QbUpdateImp and fixed problem where an extra comma was being added in QbInsertImp.

--- src/com/github/danfickle/javasqlquerybuilder/generic/QbUpdateImp.java
+++ src/com/github/danfickle/javasqlquerybuilder/generic/QbUpdateImp.java
@@ -4,34 +4,40 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import com.github.danfickle.javasqlquerybuilder.QbField;
-import com.github.danfickle.javasqlquerybuilder.QbInsert;
 
-/**
- * The default implementation of QbInsert.
- * @author DanFickle
- */
-public class QbInsertImp implements QbInsert 
+import com.github.danfickle.javasqlquerybuilder.QbField;
+import com.github.danfickle.javasqlquerybuilder.QbUpdate;
+import com.github.danfickle.javasqlquerybuilder.QbWhere;
+
+public class QbUpdateImp implements QbUpdate
 {
-	private QbInsertImp() { }
 	private String m_table;
 	private Map<String, Integer> m_placeholders;
 	private List<QbField> m_fields;
-
+	private boolean m_all;
+	private QbWhere m_where;
+	
+	private QbUpdateImp() { }
+	
 	@Override
 	public String getQueryString()
 	{
 		if (m_fields == null || m_table == null || m_placeholders == null)
 			throw new IllegalStateException("Table name or fields missing");
+
+		if (m_where == null && m_all == false)
+			throw new IllegalStateException("Must call all() to update all records");
 		
-		StringBuilder builder = new StringBuilder("INSERT INTO ");
+		StringBuilder builder = new StringBuilder("UPDATE ");
 		builder.append(QbCommonImp.protectTableName(m_table));
-		builder.append('(');
+		builder.append(" SET ");
 		
 		int fieldCnt = 0;
 		for (QbField field : m_fields)
 		{
 			builder.append(field.toString());
+			builder.append('=');
+			builder.append("?");
 
 			if (fieldCnt != m_fields.size() - 1)
 			{
@@ -40,18 +46,8 @@ public class QbInsertImp implements QbInsert
 			}
 			fieldCnt++;
 		}
-
-		builder.append(") VALUES (");
 		
-		for (int i = 0; i < m_fields.size(); i++)
-		{
-			builder.append('?');
-			builder.append(',');
-			builder.append(' ');
-		}
-
-		builder.append(')');
-		
+		builder.append(m_where.toString());
 		return builder.toString();
 	}
 
@@ -70,7 +66,7 @@ public class QbInsertImp implements QbInsert
 	}
 
 	@Override
-	public QbInsert set(QbField field, String placeholder)
+	public QbUpdate set(QbField field, String placeholder)
 	{
 		if (m_fields == null)
 			m_fields = new ArrayList<QbField>();
@@ -86,9 +82,22 @@ public class QbInsertImp implements QbInsert
 	}
 
 	@Override
-	public QbInsert inTable(String table)
+	public QbUpdate where(QbWhere where)
 	{
+		m_where = where;
+		return this;
+	}
+
+	@Override
+	public QbUpdate all() {
+		m_all = true;
+		return this;
+	}
+
+	@Override
+	public QbUpdate inTable(String table) {
 		m_table = table;
 		return this;
 	}
+
 }
