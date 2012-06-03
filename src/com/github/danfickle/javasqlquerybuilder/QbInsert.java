ddc1a2a7af871041bd01ab48f37ab21b4aa985bf
Dan Fickle <newtab@gmail.com>
2012-06-03 21:23:48 +1000

    Added the QbInsert interface.

---
+++ src/com/github/danfickle/javasqlquerybuilder/QbInsert.java
@@ -0,0 +1,23 @@
+package com.github.danfickle.javasqlquerybuilder;
+
+/**
+ * An interface to generate INSERT queries.
+ * @author DanFickle
+ */
+public interface QbInsert extends QbQuery
+{
+	/**
+	 * Sets a database column placeholder.
+	 * @param field
+	 * @param placeholder
+	 * @return This query builder.
+	 */
+	public QbInsert set(QbField field, String placeholder);
+	
+	/**
+	 * Sets the table to insert into.
+	 * @param table
+	 * @return This query builder.
+	 */
+	public QbInsert inTable(String table); 
+}
